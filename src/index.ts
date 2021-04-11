d9b330be14440cdf55ccdfad460348b3bd4d7a6c
shaunstone0 <shaunstone0@gmail.com>
2021-04-11 21:08:28 +0200

    Added exports for new error handler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export { Logger } from './logger/logger'
+export { errorHandler } from './errors/error-handler'
+export { DatabaseConnectionError } from './errors/custom-errors/database-connection-error';
