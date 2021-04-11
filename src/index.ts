5482a3d0821d368661710c1864d96425d526ecaf
shaunstone0 <shaunstone0@gmail.com>
2021-04-11 22:08:38 +0200

    removed logger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export { Logger } from './logger/logger'
 export { errorHandler } from './errors/error-handler'
 export { DatabaseConnectionError } from './errors/custom-errors/database-connection-error';
--- src/logger/logger-utils.ts
+++
@@ -1,5 +0,0 @@
-function pad(number: number): string {
-    return (number > 9 ? '' : '0') + number;
-}
-
-export default pad;
